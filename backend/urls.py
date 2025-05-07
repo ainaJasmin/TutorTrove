f3361db0a4f47a2b3afba1d0819df1f247a13c25
ainaJasmin <ainajasmin02@gmail.com>
2025-05-07 02:26:01 +0100

    Uncorrupted Submission on Github

--- backend/urls.py
+++ backend/urls.py
@@ -19,9 +19,9 @@ urlpatterns = [
     path('tutors_s/', views.getTutors, name= "tutors_s"),
     path('tutors/<str:pk>/', views.tutor, name= "tutor"),
     path('tutors_s/<str:pk>/', views.getTutor, name= "tutor_s"),
-    path('tutors_s/<str:pk>/update/', views.updateTutor, name= "update-tutor_s"),
     path('tutors_s/<str:pk>/delete/', views.api_delete_tutor, name= "delete-tutor_s"),
     path('tutorSubject_s/', views.getTutorSubject, name= "tutorSubject_s"),
+    path('tutorSubject_s_G/', views.getTutorSubject_G, name= "tutorSubject_s_G"),
     path('tutees_s/', views.getTutees, name= "tutees_s"),
     path('tuteeHours_s/', views.getTuteeHours, name="tuteeHours_s"),
     path('levels_s/', views.getLevels, name= "level_s"),
@@ -31,12 +31,14 @@ urlpatterns = [
     path('update_TS/<str:pk>/', views.updateTS, name="update_TS"),
     path('delete_TS/<str:pk>/', views.deleteTS, name="delete_TS"),
 
+
     # ---------- Frontend Authentication ----------
     path('register_s/', views.api_register),
     path('login_s/', views.api_login),
     path('logout_s/', views.api_logout),
     path('user_s/', views.api_get_user),
     path('get-csrf-token/', views.get_csrf_token),
+    path('api/user_s/<str:username>/', views.get_user_by_username, name='get_user_by_username'),
 
 
     # ---------- Invoice ----------
@@ -49,7 +51,14 @@ urlpatterns = [
     # ---------------- CRUD operatoins ------------
     path('create-tutee/', views.createTutee, name='create-tutee'),
     path('create-tutor-subject/', views.createTutorSubject, name='create-tutor-subject'),
+    path('create-tutee-hours/', views.createTuteeHours, name='create-tutee-hours'),
+    path('tutors_update/<str:pk>/', views.updateTutor, name= "update-tutor_s"),
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+
+
+
+
